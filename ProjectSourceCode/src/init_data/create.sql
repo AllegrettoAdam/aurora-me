827298e31a1b0b4b0f136760120c664c14813d7c
ulisesjc <cortez.ulises23@gmail.com>
2024-11-12 13:12:55 -0700

    added register api route and modified unit test cases

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     img VARCHAR(1000),
     username VARCHAR(16) NOT NULL,
     email VARCHAR(40) NOT NUll,
-    password VARCHAR(32) NOT NULL
+    password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE posts (
